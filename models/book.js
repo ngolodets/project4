16c840b79c563a5d6fbe1207f21851dae86bb17a
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-08-17 13:35:27 -0700

    changed book schema

--- models/book.js
+++ models/book.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: String,
   apiKey: String,
-  comment: String,
-  user: {
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User'
-  }
+  comment: String //,
+  // user: {
+  //   type: mongoose.Schema.Types.ObjectId, 
+  //   ref: 'User'
+  // }
 })
 
 module.exports = mongoose.model('Book', bookSchema);
