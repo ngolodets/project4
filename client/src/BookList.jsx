b96cb621d807c13536a931ee2b450834a299b847
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-08-20 13:34:52 -0700

    styled components in a book list

--- client/src/BookList.jsx
+++ client/src/BookList.jsx
@@ -2,33 +2,24 @@ import React from 'react';
 import axios from 'axios';
 
 function BookList({books, handleBookDetailsClick, setFavoriteBooks, displaySuggestedBooks, displayMoreBooksFromAuthor, token, addToFavorites}) {
-  
-  // function addToFavorites(title, apiKey) {
-  //   let config = {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`
-  //     }
-  //   }
-  //   axios.post('api/books', {title, apiKey}, config).then((res) => {
-  //     axios.get('api/books', config).then((response) => {
-  //       setFavoriteBooks(response.data)
-  //     })
-  //   })
-  // }
 
   let content;
+
   if (books.length) {
     content = books.map((book, index) => {
       return (
-        <div key={index} onClick={() => {displaySuggestedBooks(book.title)
-                                        displayMoreBooksFromAuthor(book.authors[0].name)}}>
-          <h4 onClick={() => handleBookDetailsClick(book.cover_edition_key)}>{book.title}</h4>
-          {book.authors && book.authors.map((author, i) => (
-            <p key={i}>{author.name}</p>
-          ))}
-          <button className='submit' id='favebutton' onClick={() => addToFavorites(book.title, book.cover_edition_key)}>ADD TO MY LIST</button>
-          <br />
-          <button className='submit' id='seemorebutton' onClick={() => displayMoreBooksFromAuthor(book.authors[0].name)}>SEE MORE FROM THIS AUTHOR</button>
+        <div key={index} className='bookinfo' onClick={() => {displaySuggestedBooks(book.title)
+                                                            displayMoreBooksFromAuthor(book.authors[0].name)}}>
+          <h5 className='booktitle' onClick={() => handleBookDetailsClick(book.cover_edition_key)}>{book.title}</h5>
+          {/* {book.authors && book.authors.map((author, i) => (
+              <p className='bookauthors' key={i}>{author.name}</p>
+          ))} */}
+          <p className='bookauthors'>{book.authors[0].name}</p>
+          <div className='buttons'>
+            <button className='submit' id='favebutton' onClick={() => addToFavorites(book.title, book.cover_edition_key)}>ADD TO MY LIST</button>
+            <br />
+            <button className='submit' id='seemorebutton' onClick={() => displayMoreBooksFromAuthor(book.authors[0].name)}>SEE MORE FROM THIS AUTHOR</button>
+          </div>
       </div>
       )
     })
